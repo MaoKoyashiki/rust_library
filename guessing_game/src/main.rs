5116cafeec164151561b0ea2359377d0fc98e6bf
UNISALMON_MAO\slowh <slowhalation62@gmail.com>
2026-05-19 20:10:46 +0900

    乱数と比較処理

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,14 @@
+use std::cmp::Ordering;
 use std::io;
+
+use rand::random_range;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = random_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
@@ -11,5 +18,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess = guess.trim().parse::<u32>().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
